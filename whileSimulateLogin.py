9f6606affae6c639785f59cbe9dfa6b8cebd9d0d
znlccy <756311868@qq.com>
2018-05-27 13:58:54 +0800

    添加Demo

---
+++ whileSimulateLogin.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+__author__ = 'znlccy znlccy@163.com'
+
+import getpass
+
+class FakeLogin(object):
+
+    def __init__(self):
+
+        self.name = 'king'
+        self.password = 'haha, no pw'
+        self.banner = 'hello, you have login system'
+        self.run()
+
+    def run(self):
+        '''仿linux终端登录窗口'''
+        print(u"不好意思，只有一个用户king")
+        print(u"偷偷告诉你，密码是6个8哦")
+        while True:
+            print(u"Login:king")
+            pw = getpass.getpass("Password:")
+            if pw == '88888888':
+                print(u"%s" %self.banner)
+                print(u"退出程序")
+                exit()
+            else:
+                if len(pw) > 12:
+                    print(u"密码长度应该小于12")
+                    continue
+                elif len(pw) < 6:
+                    print(u"密码长度应大于6才对")
+                    continue
+                else:
+                    print(u"可惜，密码错误。继续猜")
+                    continue
+
+if __name__ == '__main__':
+    fk = FakeLogin()
